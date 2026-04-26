b13890e576efa218b5b2ece3c202effcd9fe8063
YuraJE4K <68604796+YuraJE4K@users.noreply.github.com>
2026-04-26 14:53:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const API = "https://krishi-api-yr1y.onrender.com";
+
+function show(id){
+    document.querySelectorAll('.section').forEach(s => s.style.display = 'none');
+    document.getElementById(id).style.display = 'block';
+}
+
+// WEATHER
+function getWeather(){
+    let city = document.getElementById("city").value;
+
+    fetch(`${API}/weather?city=${city}`)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("weatherResult").innerText =
+        `Temp: ${data.temp}°C | Humidity: ${data.humidity} | ${data.weather}`;
+    });
+}
+
+// CROP
+function getCrop(){
+    let temp = document.getElementById("temp").value;
+    let humidity = document.getElementById("humidity").value;
+
+    fetch(`${API}/crop?temp=${temp}&humidity=${humidity}&weather=clear`)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("cropResult").innerText =
+        JSON.stringify(data.recommendations);
+    });
+}
+
+// CHAT
+function sendMsg(){
+    let msg = document.getElementById("msg").value;
+
+    fetch(`${API}/chat`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({message: msg})
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("chatResult").innerText = data.reply;
+    });
+}
